821892cd374d6b6a963fe073ac94fdc9782cbd97
k.kozlowski <k.kozlowski@reply.com>
2025-01-14 10:05:30 +0100

    feat(pom): upgrade vulnerable swagger

--- src/main/java/pl/coderion/config/SwaggerConfig.java
+++ src/main/java/pl/coderion/config/SwaggerConfig.java
@@ -8,13 +8,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Copyright (C) Coderion sp. z o.o.
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
